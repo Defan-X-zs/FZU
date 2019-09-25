25a62ff0e161c2b7c9dc8579013ef8e85dbf51d0
Zhengsheng <45557487+Defan-X-zs@users.noreply.github.com>
2019-09-25 08:44:38 +0800

    upload

---
+++ 数据结构/week4-1 银行业务队列简单模拟.cpp
@@ -0,0 +1,45 @@
+#include<iostream>//7-1 银行业务队列简单模拟	
+#include<queue>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int t;
+	queue<int> a;
+	queue<int> b;
+	for(int i = 0; i < n; i++)
+	{
+		cin>>t;
+		if(t%2==1)//奇数
+		{
+			a.push(t);
+		} 
+		else
+		{
+			b.push(t);
+		}
+	}
+	n--;
+	while(!a.empty()||!b.empty())
+	{
+		if(!a.empty()) 
+		{
+			cout<<a.front();
+			a.pop();
+			if(n){cout<<" ";n--}
+		}
+		if(!a.empty()) 
+		{
+			cout<<a.front();
+			a.pop();
+			if(n){cout<<" ";n--;}
+		}
+		if(!b.empty()) 
+		{
+			cout<<b.front();
+			b.pop();
+			if(n){cout<<" ";n--;}
+		}
+	}
+}
